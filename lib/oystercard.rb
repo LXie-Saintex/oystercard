27db4ba7ea2060f276466739abd0e73ba59d4c64
LXie-Saintex <admin@saintex.co.uk>
2020-02-26 16:15:00 +0000

    added journey history and fare

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,28 +1,60 @@
 class Oystercard
 	LIMIT = 90
 	MINIMUM = 1
-	attr_reader :balance, :entry_station
+	PENALTY = 6
+	attr_reader :balance, :journey_log, :in_journey, :current_journey
 	def initialize
 		@balance = 0
-		@entry_station = nil
+		@journey_log = []
+		@in_journey = false
 	end 
 	def top_up(amount)
 		fail "Exceeds topup limit: #{LIMIT}" if balance + amount > LIMIT
 		@balance += amount
 	end
-	def in_journey? 
-		return @entry_station ? true : false
-	end
-	def touch_in(entry_station)
+	def touch_in(station)
 		raise 'Not enough funds' if @balance < MINIMUM
-		@entry_station = entry_station
+		@in_journey = true
+		make_journey(station)
 	end
-	def touch_out
+	def touch_out(station)
 		deduct(MINIMUM)
-		@entry_station = nil
+		@in_journey  = false
+		conclude_journey(station)
 	end
 	private 
 	def deduct(amount)
 		@balance -= amount
 	end
+	def make_journey(station)
+		@current_journey = Journey.new
+		@current_journey.start_journey(station)
+	end
+	def conclude_journey(station)
+		@current_journey.finish_journey(station)
+		store_journey
+	end
+	def store_journey
+		@journey_log << @current_journey.journey
+	end	
 end
+
+ class Journey
+ 	attr_reader :journey
+  def initialize
+	  @journey = {} 
+	end
+  def start_journey(station)
+  	@journey[:entry_station] = station
+  end
+  def finish_journey(station)
+  	@journey[:exit_station] = station
+  end
+  def fare 
+  	if !@journey[:entry_station] || !@journey[:exit_station]
+  		Oystercard::PENALTY 
+  	else 
+  		Oystercard::MINIMUM
+  	end
+  end 
+ end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,7 +5,7 @@ describe Oystercard do
 		expect(subject.balance).to eq(0)
 	end
 	it "tells if in journey" do 
-		expect(subject.in_journey?).to eq(false)
+		expect(subject.in_journey).to eq(false)
 	end
 	it 'can top up' do 
 		expect(subject.top_up 20).to eq(20)
@@ -16,7 +16,7 @@ describe Oystercard do
 	it 'touches in' do 
 		subject.top_up(Oystercard::MINIMUM)
 		subject.touch_in("Farringdon")
-		expect(subject.in_journey?).to eq(true)
+		expect(subject.in_journey).to eq(true)
 	end
 	it 'prevents touch in when balance smaller than minimum' do
 		expect { subject.touch_in("Farringdon") } .to raise_error('Not enough funds')
@@ -24,18 +24,46 @@ describe Oystercard do
 	it "touches out" do
 		subject.top_up(Oystercard::MINIMUM)
 		subject.touch_in("Farringdon")
-		subject.touch_out
-		expect(subject.in_journey?). to eq(false)
+		subject.touch_out("Peckham Rye")
+		expect(subject.in_journey). to eq(false)
 	end
 	it 'deducts minimum fare' do
 		subject.top_up(Oystercard::MINIMUM)
 		subject.touch_in("Farringdon")
-		subject.touch_out
+		subject.touch_out("Peckham Rye")
 		expect(subject.balance).to eq 0
 	end
 	it "remembers the entry station" do 
 		subject.top_up(10)
 		subject.touch_in("Farringdon")
-		expect(subject.entry_station).to eq "Farringdon"
+		subject.touch_out("Liverpool Street Station")
+		expect(subject.journey_log[0][:entry_station]).to eq "Farringdon"
   end
+  it "has an empty journey log by default" do
+  	expect(subject.journey_log.length).to eq 0 
+  end
+  it "remembers all past journeys" do 
+  	subject.top_up(5)
+  	subject.touch_in("Farringdon")
+  	subject.touch_out("Peckham Rye")
+  	subject.touch_in("London Bridge Station")
+  	subject.touch_out("Bank")
+  	expect(subject.journey_log[1][:exit_station]).to eq "Bank"
+  end
+end
+
+describe Journey do 
+	it "returns fare when touched in and out" do 
+		card = Oystercard.new
+		card.top_up(5)
+		card.touch_in('Kings cross')
+		card.touch_out("Euston Square")
+		expect(card.current_journey.fare). to eq (Oystercard::MINIMUM)
+	end
+	it "returns penalty if not touched_in or touched out" do
+		card = Oystercard.new
+		card.top_up(5)
+		card.touch_in('Farringdon')
+		expect(card.current_journey.fare). to eq (Oystercard::PENALTY)
+	end
 end
